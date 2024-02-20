3b7f7edb0182fa81ad5db6137c357544736cae09
houssem-eddinjallouli <houssem-eddin.jallouli@esprit.tn>
2024-02-20 18:31:44 +0100

    FFFFF

---
+++ codemastersbackend/src/main/java/tn/esprit/codemasters/service/houssem/TestServiceImp.java
@@ -0,0 +1,14 @@
+package tn.esprit.codemasters.service.houssem;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import tn.esprit.codemasters.entity.Test;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TestServiceImp implements ITestService{
+
+
+}
